7957d9d438a8395b2d33243004fc5ddda1efb465
Chemaclass <chemaclass@outlook.es>
2024-05-24 11:49:48 +0200

    upgrade phel 0.14

--- phel-config.php
+++ phel-config.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 use Phel\Config\PhelConfig;
 use Phel\Config\PhelExportConfig;
-use Phel\Config\PhelOutConfig;
+use Phel\Config\PhelBuildConfig;
 
 return (new PhelConfig())
     ->setSrcDirs(['src'])
     ->setTestDirs(['tests'])
-    ->setOut((new PhelOutConfig())
+    ->setBuildConfig((new PhelBuildConfig())
         ->setMainPhelNamespace('cli-skeleton\main')
         ->setMainPhpPath('out/main.php'))
     ->setFormatDirs(['src', 'tests'])
-    ->setExport((new PhelExportConfig())
-        ->setDirectories(['src/modules'])
+    ->setExportConfig((new PhelExportConfig())
+        ->setFromDirectories(['src/modules'])
         ->setNamespacePrefix('PhelGenerated')
         ->setTargetDirectory('src/PhelGenerated'))
     ->setIgnoreWhenBuilding(['local.phel'])
