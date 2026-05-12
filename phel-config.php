919ace09b1165d4ebee8c359719f470b65cdd138
Jesus Valera Reales <github@jesusvalera.dev>
2026-05-12 13:18:32 +0200

    Update PhelConfig method call to use named arguments

--- phel-config.php
+++ phel-config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Phel\Config\PhelConfig;
 
-return PhelConfig::forProject('cli-skeleton.main')
+return PhelConfig::forProject(mainNamespace: 'cli-skeleton.main')
     ->withMainPhpPath('out/main.php')
     ->withIgnoreWhenBuilding(['local.phel'])
     ->withExportFromDirectories(['src/modules']);
